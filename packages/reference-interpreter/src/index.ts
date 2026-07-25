bbd14a81e3d896575157bcb37e74c73e45bdbafa
ChriX-Goh <Chrix.holy@gmail.com>
2026-07-25 12:26:09 +0800

    feat(interpreter): export graph evaluator

--- packages/reference-interpreter/src/index.ts
+++ packages/reference-interpreter/src/index.ts
@@ -1,10 +1,15 @@
 export { evaluateSwitchProfile } from './evaluate.js';
+export { evaluateProfileGraph } from './graph.js';
 export { ipMatchesPrefix, isIpLiteral, parseIpLiteral } from './ip.js';
 export { matchCondition } from './match.js';
 export type {
   ConditionMatchResult,
+  GraphDecision,
+  GraphEvaluationOptions,
+  GraphTraceEntry,
   ReferenceRequest,
   ReferenceSupport,
+  ResolvedReferenceRoute,
   RuleTraceEntry,
   SwitchDecision,
 } from './types.js';
