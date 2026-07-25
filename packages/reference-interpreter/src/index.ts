9f7a51187a99a892a4f798f636afd2467f01188c
ChriX-Goh <Chrix.holy@gmail.com>
2026-07-25 12:32:43 +0800

    feat(interpreter): export rule-list parser and evaluator

--- packages/reference-interpreter/src/index.ts
+++ packages/reference-interpreter/src/index.ts
@@ -2,14 +2,20 @@ export { evaluateSwitchProfile } from './evaluate.js';
 export { evaluateProfileGraph } from './graph.js';
 export { ipMatchesPrefix, isIpLiteral, parseIpLiteral } from './ip.js';
 export { matchCondition } from './match.js';
+export { evaluateRuleListProfile, parseRuleList } from './rule-list.js';
 export type {
   ConditionMatchResult,
   GraphDecision,
   GraphEvaluationOptions,
   GraphTraceEntry,
+  ParsedRuleListRule,
   ReferenceRequest,
   ReferenceSupport,
   ResolvedReferenceRoute,
+  RuleListDecision,
+  RuleListParseResult,
+  RuleListPriorityGroup,
+  RuleListTraceEntry,
   RuleTraceEntry,
   SwitchDecision,
 } from './types.js';
