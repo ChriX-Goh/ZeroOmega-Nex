399c5d8f6ce40d416973d19a66f479c78452d06a
ChriX-Goh <Chrix.holy@gmail.com>
2026-07-25 12:58:29 +0800

    feat(pac): export capability analyzer

---
+++ packages/pac-compiler/src/index.ts
@@ -0,0 +1,10 @@
+export { analyzePacCompatibility } from './capabilities.js';
+export {
+  PAC_COMPILER_VERSION,
+  type PacCapability,
+  type PacCapabilityAnalysis,
+  type PacCapabilityIssue,
+  type PacCapabilitySummary,
+  type PacIssueSeverity,
+  type PacTarget,
+} from './contracts.js';
