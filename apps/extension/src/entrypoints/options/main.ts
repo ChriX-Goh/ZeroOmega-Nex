4471da5bf6cc6e261ed30f34c819928726c98cc1
ChriX-Goh <Chrix.holy@gmail.com>
2026-07-26 07:06:44 +0800

    feat: localize Options from system language

--- apps/extension/src/entrypoints/options/main.ts
+++ apps/extension/src/entrypoints/options/main.ts
@@ -1,4 +1,6 @@
 import { mount } from 'svelte';
+
+import { localizeDocument } from '../../lib/i18n';
 import App from './App.svelte';
 import './style.css';
 
@@ -8,3 +10,4 @@ if (!target) {
 }
 
 mount(App, { target });
+localizeDocument();
