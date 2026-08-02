af0e5e559b2e79962247c1225edb61b2d65c9061
ChriX-Goh <Chrix.holy@gmail.com>
2026-08-02 16:01:36 +0800

    fix: open Options on original About page

--- apps/extension/src/entrypoints/options/main.ts
+++ apps/extension/src/entrypoints/options/main.ts
@@ -3,6 +3,9 @@ import { mount } from 'svelte';
 import { localizeDocument } from '../../lib/i18n';
 import App from './App.svelte';
 import './style.css';
+import './original-compat.css';
+
+if (!window.location.hash) window.history.replaceState(null, '', '#/about');
 
 const target = document.getElementById('app');
 if (!target) {
